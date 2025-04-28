39081301ac0a4f0385dce1ad34220d42216478f2
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-28 12:46:52 -0500

    Added example string Unit Tests.

---
+++ EmployeeManagement.Test/EmployeeTests.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstAndLastName_FullNameIsConcatenated()
+        {
+            // Arrange
+            var employee = new InternalEmployee("John", "Doe", 1, 2000, false, 1);
+            // Act
+            var fullName = employee.FullName;
+            // Assert
+            Assert.Equal("John Doe", fullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstAndLastName_FullNameStartsWithFirstName()
+        {
+            // Arrange
+            var employee = new InternalEmployee("John", "Doe", 1, 2000, false, 1);
+            // Act
+            var fullName = employee.FullName;
+            // Assert
+            Assert.StartsWith(employee.FirstName, fullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstAndLastName_FullNameEndsWithLastName()
+        {
+            // Arrange
+            var employee = new InternalEmployee("John", "Doe", 1, 2000, false, 1);
+            // Act
+            var fullName = employee.FullName;
+            // Assert
+            Assert.EndsWith(employee.LastName, fullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstAndLastName_FullNameSoundsLikeConcatenation()
+        {
+            // Arrange
+            var employee = new InternalEmployee("John", "Doe", 1, 2000, false, 1)
+            {
+                // Act
+                FirstName = "Lucia",
+                LastName = "Shelton"
+            };
+            // Assert
+            Assert.Matches("Lu(c|s|z)ia Shel(t|d)on", employee.FullName);
+        }
+    }
+}
