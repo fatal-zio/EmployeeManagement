f2e5bb0eae2bb57a99e551ead02b371fd10a3811
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-28 12:22:32 -0500

    Added first Unit Test.

---
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+
+            Assert.Equal(2500, employee.Salary);
+        }
+    }
+}
