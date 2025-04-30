a963bdef02c3c5e293b9bb9acdcd16e6ec0c0437
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-30 17:39:13 -0500

    Added more tests.

--- EmployeeManagement.Test/EmployeeServiceTests.cs
+++ EmployeeManagement.Test/EmployeeServiceTests.cs
@@ -33,5 +33,37 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.Contains(internalEmployee.AttendedCourses, course => course.Id == Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"));
         }
+
+        [Fact]
+        public void CreateInternalImployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourses()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeService = new EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+            var obligatoryCourses = employeeManagementTestDataRepository.GetCourses(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"),
+                Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+
+            // Act
+            var internalEmployee = employeeService.CreateInternalEmployee("Brooklyn", "Cannon");
+
+            // Assert
+            Assert.Equal(obligatoryCourses, internalEmployee.AttendedCourses);
+        }
+
+        [Fact]
+        public void CreateInternalImployee_InternalEmployeeCreated_AttendedCoursesMustNotBeNew()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeService = new EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+            var obligatoryCourses = employeeManagementTestDataRepository.GetCourses(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"),
+                Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+
+            // Act
+            var internalEmployee = employeeService.CreateInternalEmployee("Brooklyn", "Cannon");
+
+            // Assert
+            Assert.All(internalEmployee.AttendedCourses, course => Assert.False(course.IsNew));
+        }
     }
 }
