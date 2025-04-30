7c6f621cb2fb83648f04abc7e231d173927a3f35
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-30 17:56:46 -0500

    More tests.

--- EmployeeManagement.Test/EmployeeServiceTests.cs
+++ EmployeeManagement.Test/EmployeeServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeManagement.Business;
+using EmployeeManagement.Business.Exceptions;
+using EmployeeManagement.DataAccess.Entities;
 using EmployeeManagement.Test.Services;
 
 namespace EmployeeManagement.Test
@@ -64,6 +66,36 @@ namespace EmployeeManagement.Test
 
             // Assert
             Assert.All(internalEmployee.AttendedCourses, course => Assert.False(course.IsNew));
+        }
+
+        [Fact]
+        public async Task CreateInternalImployee_InternalEmployeeCreated_MustHaveAttendedFirstObligatoryCourses_Async()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository = new EmployeeManagementTestDataRepository();
+            var employeeService = new EmployeeService(employeeManagementTestDataRepository, new EmployeeFactory());
+            var obligatoryCourses = await employeeManagementTestDataRepository.GetCoursesAsync(Guid.Parse("37e03ca7-c730-4351-834c-b66f280cdb01"),
+                Guid.Parse("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e"));
+
+            // Act
+            var internalEmployee = await employeeService.CreateInternalEmployeeAsync("Brooklyn", "Cannon");
+
+            // Assert
+            Assert.Equal(obligatoryCourses, internalEmployee.AttendedCourses);
+        }
+
+        [Fact]
+        public async Task GiveRaise_RaiseBelowMinimumGiven_EmployeeInvalidRaiseExceptionMustBeThrown()
+        {
+            // Arrange
+            var employeeService = new EmployeeService(new EmployeeManagementTestDataRepository(), new EmployeeFactory());
+            var internalEmployee = new InternalEmployee("Brook", "Cannon", 5, 3000, false, 1);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<EmployeeInvalidRaiseException>(async () =>
+            {
+                await employeeService.GiveRaiseAsync(internalEmployee, 50);
+            });
         }
     }
 }
