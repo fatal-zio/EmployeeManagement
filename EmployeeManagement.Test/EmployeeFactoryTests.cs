96a7f1641f9458b04df6ac19de4e48fb5a0698fc
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-30 13:52:06 -0500

    Added more example Tests.

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -13,5 +13,24 @@ namespace EmployeeManagement.Test
 
             Assert.Equal(2500, employee.Salary);
         }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+
+            Assert.InRange(employee.Salary,2500, 3500);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+        {
+            var employeeFactory = new EmployeeFactory();
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+            employee.Salary = 2500.123m;
+
+            Assert.Equal(2500, employee.Salary, 0);
+        }
     }
 }
