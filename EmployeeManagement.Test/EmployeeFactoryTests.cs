4afdba7c688199ba48bed431ccc3a29d1c71b7c6
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-05-21 10:20:59 -0500

    Moved repeated logic to a constructor.

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -5,11 +5,17 @@ namespace EmployeeManagement.Test
 {
     public class EmployeeFactoryTests
     {
+        private readonly EmployeeFactory _employeeFactory;
+
+        public EmployeeFactoryTests()
+        {
+            _employeeFactory = new EmployeeFactory();
+        }
+
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Jayme", "Desrosiers");
 
             Assert.Equal(2500, employee.Salary);
         }
@@ -17,8 +23,7 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Jayme", "Desrosiers");
 
             Assert.InRange(employee.Salary,2500, 3500);
         }
@@ -26,8 +31,7 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Jayme", "Desrosiers");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Jayme", "Desrosiers");
             employee.Salary = 2500.123m;
 
             Assert.Equal(2500, employee.Salary, 0);
@@ -36,8 +40,7 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_IsExternalIsTrue_ReturnTypeMustBeExternalEmployee()
         {
-            var employeeFactory = new EmployeeFactory();
-            var employee = employeeFactory.CreateEmployee("Jayme", "Desrosiers", "Company", true);
+            var employee = _employeeFactory.CreateEmployee("Jayme", "Desrosiers", "Company", true);
             Assert.IsType<ExternalEmployee>(employee);
         }
     }
